354d189d91d68ccc5ddba21d462d6fcc7f1c773c
Sergey Kibish <sergey.kibish@gmail.com>
2014-12-07 01:06:00 +0000

    merge

--- bio/main/view.js
+++ bio/main/view.js
@@ -35,15 +35,16 @@ var map = AmCharts.makeChart("mapdiv", {
     dataProvider: {
         map: "continentsLow",
         getAreasFromMap: true,
-        images: [{
-            zoomLevel: 5,
-            scale: 0.5,
-            title: "Brussels",
-            latitude: 50.8371,
-            longitude: 4.3676
-        }]
+            areas: [
+        {
+		  id: "europe",
+		  title: "<h1>Holy</h1>",
+		}
+    ],
         
     },
+    
+
     customData: {test:3},
     
     areasSettings: {
@@ -53,8 +54,8 @@ var map = AmCharts.makeChart("mapdiv", {
         smallMap: {}
 });
 
-map.addListener('clickMapObject', function(event) {
-    console.log(event.mapObject);
+map.addListener('rollOverMapObject', function(event) {
+    console.log(event.mapObject.chart.customData);
     map.zoomToGroup(event.mapObject.groupId);
 });
 
@@ -107,64 +108,3 @@ $this.find('#chat-input').on("keypress", function(event){
     });
     
 })();
-
-
-
-function updateCustomMarkers () {
-    // get map object
-    
-    console.log(map);
-    return;
-    var map = event.chart;
-    
-    // go through all of the images
-    for( var x in map.dataProvider.images) {
-        // get MapImage object
-        var image = map.dataProvider.images[x];
-        
-        // check if it has corresponding HTML element
-        if ('undefined' == typeof image.externalElement)
-            image.externalElement = createCustomMarker(image);
-
-        // reposition the element accoridng to coordinates
-        image.externalElement.style.top = map.latitudeToY(image.latitude) + 'px';
-        image.externalElement.style.left = map.longitudeToX(image.longitude) + 'px';
-    }
-}
-
-
-// this function creates and returns a new marker element
-function createCustomMarker(image) {
-    // create holder
-    var holder = document.createElement('div');
-    holder.className = 'map-marker';
-    holder.title = image.title;
-    holder.style.position = 'absolute';
-    
-    // maybe add a link to it?
-    if (undefined != image.url) {
-        holder.onclick = function() {
-            window.location.href = image.url;
-        };
-        holder.className += ' map-clickable';
-    }
-    
-    // create dot
-    var dot = document.createElement('div');
-    dot.className = 'dot';
-    holder.appendChild(dot);
-    
-    // create pulse
-    var pulse = document.createElement('div');
-    pulse.className = 'pulse';
-    holder.appendChild(pulse);
-    
-    // append the marker to the map container
-    image.chart.chartDiv.appendChild(holder);
-    
-    image.externalElement.style.top = map.latitudeToY(image.latitude) + 'px';
-    image.externalElement.style.left = map.longitudeToX(image.longitude) + 'px';
-}
-
-console.log(map.dataProvider.images)
-
